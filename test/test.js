959cc705b8966a54d1db7f4ebcaee0bf18add30f
Steven R. Loomis <srloomis@us.ibm.com>
2015-01-28 17:42:04 -0800

    i seem to be depending on broken libs.
     https://github.com/signalfx/swagger-node-client/issues/1

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 // TODO: store this in the creds file, VCAP, etc
-var projectId = 'MyProject';
-var apiKey = '34EF-45FF-AB12-DC78';
-//var url = 'http://localhost:9131/translate/';
-var url = 'http://ultra6.sanjose.ibm.com:9080/translation-service/v1/';
+var projectId = process.env.TAAS_PROJECT || 'MyProject';
+var apiKey = process.env.TAAS_API_KEY || '34EF-45FF-AB12-DC78';
+var url = process.env.TAAS_API_URL || 'http://ultra6.sanjose.ibm.com:9080/translate/';
 
 var taas = require('../index.js')({ url: url, api: apiKey, project: projectId });
-
+/*
+var taas = require('./index.js')({ url: url, api: apiKey, project: projectId });
+*/
 var sourceLoc = "en-US";
 var targLoc = "zh-Hans";
 
@@ -15,9 +16,11 @@ var sourceData = [
 ];
 
 
-taas.translate({source: sourceLoc, target: targLoc, data: sourceData}, function OKAY(x) {
-    console.log('-'); console.log(x); console.log("RESPONSE: " + JSON.stringify(x.entity));
-} , function ERR(x){ console.log("ERR:"); console.log(x);  });
+//taas.translate({source: sourceLoc, target: targLoc, data: sourceData}, function OKAY(x) {
+//    console.log('-'); console.log(x); console.log("RESPONSE: " + JSON.stringify(x.entity));
+//} , function ERR(x){ console.log("ERR:"); console.log(x);  });
+
+taas.schema(console.log, console.err, []); // 
 
 // Local Variables:
 // compile-command: "cd c:/Users/IBM_ADMIN/git/taas-nodejs-client/ ; npm run test"
